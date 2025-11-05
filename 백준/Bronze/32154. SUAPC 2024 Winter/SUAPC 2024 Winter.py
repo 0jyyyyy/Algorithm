c02b0c7def52c10cfde46175138f286b85034ca9
Oh Jun Yeong <88613651+0jyyyyy@users.noreply.github.com>
2025-11-05 23:35:13 +0900

    [Bronze V] Title: SUAPC 2024 Winter, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/32154. SUAPC 2024 Winter/SUAPC 2024 Winter.py
@@ -0,0 +1,16 @@
+score_board = {
+    1: (11, 'A B C D E F G H J L M'),
+    2: (9, 'A C E F G H I L M'),
+    3: (9, 'A C E F G H I L M'),
+    4: (9, 'A B C E F G H L M'),
+    5: (8, 'A C E F G H L M'),
+    6: (8, 'A C E F G H L M'),
+    7: (8, 'A C E F G H L M'),
+    8: (8, 'A C E F G H L M'),
+    9: (8, 'A C E F G H L M'),
+    10: (8, 'A B C F G H L M')
+}
+
+N = int(input())
+print(score_board[N][0])
+print(score_board[N][1])
