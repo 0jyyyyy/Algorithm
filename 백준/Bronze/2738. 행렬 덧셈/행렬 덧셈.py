a3e7af47dc01ba2e95e700c5184f6d99e84b93d7
Oh Jun Yeong <88613651+0jyyyyy@users.noreply.github.com>
2025-11-15 17:17:53 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -0,0 +1,9 @@
+N,M = map(int, input().split())
+
+matrix_1 = [list(map(int,input().split()))for _ in range(N)]
+matrix_2 = [list(map(int,input().split()))for _ in range(N)]
+
+for i in range(N):
+  for j in range(M):
+    print(matrix_1[i][j]+matrix_2[i][j], end=" ")
+  print()
