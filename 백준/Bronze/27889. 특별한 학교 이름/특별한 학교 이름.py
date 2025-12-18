3f904c88bfafaf01b523158ecca86b78cf32938b
Oh Jun Yeong <88613651+0jyyyyy@users.noreply.github.com>
2025-12-18 16:38:56 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,8 @@
+GEC = {"NLCS": 'North London Collegiate School',
+       "BHA" : 'Branksome Hall Asia',
+       'KIS' : 'Korea International School',
+       'SJA' : 'St. Johnsbury Academy'
+       }
+
+n = input()
+print(GEC[n])
